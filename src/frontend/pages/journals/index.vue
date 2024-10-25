3b4564c07bfcfc039df244b2a880c3bfac8f2aa4
David Lin <dlin88@binghamton.edu>
2024-10-25 16:41:31 -0400

    Create basic page

---
+++ src/frontend/pages/journals/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+import MainShell from "~/layouts/MainShell.vue";
+
+</script>
+
+<template>
+  <MainShell>
+    Content inside
+  </MainShell>
+</template>
+
+<style scoped>
+
+</style>
