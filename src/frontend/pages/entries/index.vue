8f1779cd3f3096859c9e7ee76308ac64d235958a
David Lin <dlin88@binghamton.edu>
2024-10-25 22:23:20 -0400

    Print out journal entries

---
+++ src/frontend/pages/entries/index.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+import MainShell from "~/layouts/MainShell.vue";
+
+const {data: entries} = await useFetch("http://localhost:8080/entries");
+</script>
+
+<template>
+  <MainShell>
+    <pre>{{entries}}</pre>
+
+    <div v-if="entries">
+      <ul>
+        <li v-for="entry in entries" :key="entry.entryId">
+          Title: {{ entry.title }}
+          <br>
+          Body: {{ entry.body }}
+          <br>
+          createdAt: {{ entry.createdAt }}
+          <br>
+          modifiedAt: {{ entry.modifiedAt }}
+          <br>
+          Journal: {{ entry.journalName }}
+          <br>
+          Tags: {{ entry.tags }}
+        </li>
+      </ul>
+    </div>
+  </MainShell>
+</template>
+
+<style scoped>
+
+</style>
