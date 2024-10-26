bd19027f59491d167517255aa19266d728d6666d
David Lin <dlin88@binghamton.edu>
2024-10-25 22:08:08 -0400

    Print out journal information

--- src/frontend/pages/journals/index.vue
+++ src/frontend/pages/journals/index.vue
@@ -2,11 +2,30 @@
 
 import MainShell from "~/layouts/MainShell.vue";
 
+const {data: journals} = await useFetch("http://localhost:8080/journals");
 </script>
 
 <template>
   <MainShell>
-    Content inside
+    <pre>{{journals}}</pre>
+
+    <div v-if="journals">
+      <ul>
+        <li v-for="journal in journals" :key="journal.journalId">
+          Name: {{ journal.name }}
+          <br>
+          Description: {{ journal.description }}
+          <br>
+          createdAt: {{ journal.createdAt }}
+          <br>
+          modifiedAt: {{ journal.modifiedAt }}
+          <br>
+          journalId: {{ journal.journalId }}
+          <br>
+          Entries: {{ journal.entries }}
+        </li>
+      </ul>
+    </div>
   </MainShell>
 </template>
 
