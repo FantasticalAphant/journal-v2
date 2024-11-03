a686a8983c56e358e2ace71293b02a73ae82c906
David Lin <dlin88@binghamton.edu>
2024-11-02 23:35:20 -0400

    Create new entry list view

--- src/frontend/pages/entries/index.vue
+++ src/frontend/pages/entries/index.vue
@@ -10,27 +10,9 @@ const {data: entries} = await useFetch("http://localhost:8080/entries");
       title="Entries"
       :path="$route.path"
   >
-    <pre>{{entries}}</pre>
+    <EntryListView :entries="entries"/>
 
-    <div v-if="entries">
-      <ul>
-        <li v-for="entry in entries" :key="entry.entryId">
-          <NuxtLink class="text-blue-600" :to="`/entries/${entry.entryId}`">
-            Title: {{ entry.title }}
-          </NuxtLink>
-          <br>
-          Body: {{ entry.body }}
-          <br>
-          createdAt: {{ entry.createdAt }}
-          <br>
-          modifiedAt: {{ entry.modifiedAt }}
-          <br>
-          Journal: {{ entry.journalName }}
-          <br>
-          Tags: {{ entry.tags }}
-        </li>
-      </ul>
-    </div>
+    <pre>{{entries}}</pre>
   </MainShell>
 </template>
 
