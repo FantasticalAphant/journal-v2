2d5cb61e2d2b12c52ae153c966582d94e087db13
David Lin <dlin88@binghamton.edu>
2024-11-03 23:26:44 -0500

    Allow user to create new tags

--- src/frontend/pages/tags/index.vue
+++ src/frontend/pages/tags/index.vue
@@ -2,12 +2,35 @@
 
 import MainShell from "~/layouts/MainShell.vue";
 const {data: tags} = await useFetch("http://localhost:8080/tags");
+
+const tagName = ref("");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  await useFetch("http://localhost:8080/tags", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      tagName: tagName.value,
+    })
+  });
+
+  journalName.value = "";
+}
+
 </script>
 
 <template>
   <MainShell
       title="Tags"
       :path="$route.path">
+    <form @submit="handleSubmit" class="flex max-w-xs gap-2 mb-3">
+      <input v-model="tagName" placeholder="Name" class="pl-1 border-2 rounded">
+      <button type="submit" class="bg-blue-500 text-white p-1 rounded">Submit</button>
+    </form>
     <pre>{{tags}}</pre>
     <div v-if="tags">
       <ul>
