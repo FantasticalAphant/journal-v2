5fcd163ae553026113bcf58bf211212aca8946f1
David Lin <davidlin9812@gmail.com>
2024-12-10 21:47:31 -0500

    Add map using Leaflet

--- src/frontend/pages/map/index.vue
+++ src/frontend/pages/map/index.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
 
 import MainShell from "~/layouts/MainShell.vue";
+import {LMap, LTileLayer} from "@vue-leaflet/vue-leaflet";
+import 'leaflet/dist/leaflet.css'
+
+const zoom = ref(8);
 </script>
 
 <template>
   <MainShell
       title="Map"
       :path="$route.path">
+
+    <div style="height:75vh; width:75vw">
+      <LMap
+          ref="map"
+          :center="[38.9072, -77.0369]"
+          :use-global-leaflet="false"
+          :zoom="zoom"
+      >
+        <LTileLayer
+            attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+            layer-type="base"
+            name="OpenStreetMap"
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </LMap>
+    </div>
+
   </MainShell>
 </template>
 
